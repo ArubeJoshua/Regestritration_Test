4b4c7281d4c1353e5407f8f56e325923880df6b6
Arube Joshua <arubejoshua85@gmail.com>
2024-05-31 14:54:55 +0300

    Adding the Entry point

---
+++ registration_system/src/main/java/com/joshua/entry/Main.java
@@ -0,0 +1,19 @@
+package com.joshua.entry;
+
+public class Main {
+    public static void main(String[] args) {
+        menuOp();
+    }
+
+    public static void menuOp() {
+        System.out.println("......Welcome to the User Registration System......");
+        System.out.println("Select your required service: ");
+        System.out.println("1. Register User");
+        System.out.println("2. List all Users");
+        System.out.println("3. Get a user by username");
+        System.out.println("4. Delete all users");
+        System.out.println("5. Delete a user by username");
+        System.out.println("6. Update user details");
+        System.out.println("7. Exit");
+    }
+}
